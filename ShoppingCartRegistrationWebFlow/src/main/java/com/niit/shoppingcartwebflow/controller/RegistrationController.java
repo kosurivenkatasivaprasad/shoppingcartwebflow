94266d3bc6784c9a693c25e5a8e0d2d08b33d50f
kosurivenkatasivaprasad <venkatasivaprasad.kosuri@gmail.com>
2016-08-23 07:49:57 +0530

    Add files via upload
    its a registration web flow

---
+++ ShoppingCartRegistrationWebFlow/src/main/java/com/niit/shoppingcartwebflow/controller/RegistrationController.java
@@ -0,0 +1,23 @@
+package com.niit.shoppingcartwebflow.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+
+@Controller
+public class RegistrationController {
+
+	@RequestMapping("home")
+	
+	public String home(){
+		return "index";
+	}
+	
+	
+}
+
+
